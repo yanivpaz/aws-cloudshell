3cfcf6bdd0988ff184126bc602eea3cc630c56de
Yaniv Paz <yanivpaz1973@gmail.com>
2021-01-19 12:52:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
+# curl -s https://raw.githubusercontent.com/yanivpaz/aws-cloudshell/main/install.sh |bash
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+aws eks list-clusters --output text | awk '{print $2}'
 
 
