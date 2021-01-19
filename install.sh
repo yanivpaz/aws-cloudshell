34a82519fc6040dcbdb969fef7eef2bdd5c0616b
Yaniv Paz <yanivpaz1973@gmail.com>
2021-01-19 12:58:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,4 +13,7 @@ EKS_CLUSTER=`aws eks list-clusters --output text | awk '{print $2}'|tail -1`
 aws eks update-kubeconfig --name $EKS_CLUSTER
 
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 
