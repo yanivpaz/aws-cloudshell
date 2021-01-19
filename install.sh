e5a67bf0b3461a1f2553ea120ff7ae7a2d8ad54c
Yaniv Paz <yanivpaz1973@gmail.com>
2021-01-19 14:47:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ fi
 if [ ! -f /usr/local/bin/eksctl ]
 then
  echo "Installing eksctl"
- curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+ curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
  sudo mv /tmp/eksctl /usr/local/bin
 fi 
 
